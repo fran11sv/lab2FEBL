ceb8c0cebb3cbd4752d19114782ab3719cd734d4
fran11sv <fran11sv@gmail.com>
2021-04-28 01:12:39 -0600

    Parcial 2 Francisco Barrientos
    CRUD en Laravel 8.0.3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController; 
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('productos', 'HomeController@indexprod')->name('productos');
+//Route::get('/productos', [App\Http\Controllers\HomeController::class, 'indexprod'])->name('productos');
+//Route::resource('productos', 'ProductosController');
+Route::resource('productos', ProductosController::class);
